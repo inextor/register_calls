28589906e678db8d02ae34ffefa6904b1f6deebd
Nextor Leon <nextor@gmail.com>
2019-07-25 11:42:56 -0700

    Es6 version

--- js/background.js
+++ js/background.js
@@ -1,6 +1,18 @@
+import Finger from './Finger/DatabaseStore.js';
+import Server from './Extension-Framework/Server.js';
+import config from './config.js';
+import database_schema from './database_schema.js';
+import PromiseUtil from '../PromiseUtils/PromiseUtil.js';
 
-var missedCalls				= {};
-var ext						= new ExtensionFrameworkServer();
+window.onerror = function(err){
+    console.log(err); // logs all errors
+};
+
+let db = new Finger( database_schema );
+
+db.init().then(()=> console.log('DB initialized'));
+
+var ext			= new Server();
 
 ext.addPageLoadListener( config.load_listener ,true,()=>
 {
@@ -8,112 +20,42 @@ ext.addPageLoadListener( config.load_listener ,true,()=>
 });
 
 var init    = new Date();
+var last_call = new Date();
 
-if( config.debug )
-	var init    = new Date('2017-01-01 00:00:00'); 
+setInterval(()=>
+{
+	let current = new Date();
+	current.setMinutes( current.getMinutes() -5 );
+
+	if( last_call <  current );
+	{
+		makeCallBacks().catch((e)=>{
+			console.log( e );
+		});
+	}
+
+}, 60*1000 );
+
+
 
 ext.addCustomRequestListener('calls_found',(url,request,tab_id)=>
 {
 	console.log('Call Missed found', request, new Date() );
-	/*
-	 *Array(3)
-	Array(3)
-	qa-call-id : "114111378371"
-	qa-charge : ""
-	qa-datetime : "August 28, 2017 13:47"
-	qa-from : "+1 (202) 997-5097"
-	qa-hold-time : "00:00"
-	qa-minutes : "1"
-	qa-status : "Abandoned In Queue"
-	qa-ticket-id : ""
-	qa-to : "+1 (213) 205-0042"
-	qa-wait-time : "00:17"
-	qa-wrap-up-time : "00:00"
-	*/
-
-	if( Array.isArray( request ) )
-	{
-		let calls	= [];
-
-		request.forEach(i=>
-		{
-            let d       = new Date( i['qa-datetime'] );
-
-			var callInfo	= {
-				id				: i['qa-call-id']
-				,ticket_id		: i['qa-ticket-id']
-				,date			: getUTCMysqlTimestamp( i['qa-datetime'] )
-				,local_time		: getMysqlTimestamp( i['qa-datetime'] )
-				,to				: i['qa-to']
-				,from			: i['qa-from']
-				,agent			: i['qa-agent']
-				,call_status	: i['qa-status']
-				,wait_time		: i['qa-wait-time']
-				,hold_time		: i['qa-hold-time']
-				,wrap_up_time	: i['qa-wrap-up-time']
-				,minutes		: i['qa-minutes']
-				,charge			: i['qa-charge']
-			};
-
-			calls.push( callInfo );
-
-			if( typeof missedCalls[ i['qa-call-id'] ] === 'undefined' && d > init  && i['qa-status'] == 'Abandoned In Queue' )
-			{
-				missedCalls[ i['qa-call-id'] ] = 1;
-                let date = getDateString( i['qa-datetime'] );
-                
-
-				var promise = axhrw
-				({
-
-					method				: 'POST'			//,'POST',...
-					,url				: config.create_ticket_url		//Required
-					//,headers			: { "connection"	: config.http_connection }
-					,data				:
-					{
-						title			: 'Missed Call From '+i['qa-from']+' '+date
-						,message			: 'Missed Call From '+i['qa-from']+' '+date
-									+'\nCall Status: '+i['qa-status']
-									+'\nCall To: '+i['qa-to']
-                                    +'\nWait Time: '+i['qa-wait-time']
-                                    +'\nHold Time: '+i['qa-hold-time']
-					}
-				})
-				.then((response)=>
-				{
-					console.log( response );
-				})
-				.catch((reason)=>
-				{
-					console.log('Fails to send to server');
-				});
-			}
-		});
-
-		axhrw
-		({
-			method	: 'POST'				//,'POST',...
-			,url	: config.add_calls_url	//Required
-			,data	: { 'calls' : calls }
-		})
-		.then((response)=>
-		{
-			
-		});
-	}
+	processCallsFound( requests );
 });
 
 
 function getUTCMysqlTimestamp( str )
 {
     let d       = new Date(str);
+	let z_number = n=> n > 9 ? n : '0'+n;
 
-	let month	= d.getUTCMonth()<9 ? '0'+(d.getUTCMonth()+1):(d.getUTCMonth()+1);
-	let days	= d.getUTCDate() < 10 ? '0'+d.getUTCDate() : d.getUTCDate();
+	let month	= z_number( d.getUTCMonth()+1);
+	let days	= z_number( d.getUTCDate() );
 
-    let seconds = d.getUTCSeconds() < 10 ? '0'+d.getUTCSeconds() : d.getUTCSeconds();
-    let min     = d.getUTCMinutes() < 10 ? '0'+d.getUTCMinutes() : d.getUTCMinutes();
-    let hour    = d.getUTCHours()<10 ? '0'+d.getUTCHours() : d.getUTCHours();
+    let seconds = z_number( d.getUTCSeconds() );
+    let min     = z_number( d.getUTCMinutes() );
+    let hour    = z_number( d.getUTCHours() );
 
 	return d.getFullYear()+'-'+month+'-'+days+' '+hour+':'+min+':'+seconds;
 }
@@ -121,13 +63,14 @@ function getUTCMysqlTimestamp( str )
 function getMysqlTimestamp( str )
 {
     let d       = new Date(str);
+	let z_number = n=> n > 9 ? n : '0'+n;
 
-	let month	= d.getMonth()<9 ? '0'+(d.getMonth()+1):(d.getMonth()+1);
-	let days	= d.getDate()  < 10 ? '0'+d.getDate() : d.getDate();
+	let month	= z_number( d.getMonth()+1);
+	let days	= z_number( d.getDate() );
 
-    let seconds = d.getSeconds() < 10 ? '0'+d.getSeconds() : d.getSeconds();
-    let min     = d.getMinutes() < 10 ? '0'+d.getMinutes() : d.getMinutes();
-    let hour    = d.getHours()<10 ? '0' + d.getHours() : d.getHours();
+    let seconds = z_number( d.getSeconds() );
+    let min     = z_number( d.getMinutes() );
+    let hour    = z_number( d.getHours() );
 
 	return d.getFullYear()+'-'+month+'-'+days+' '+hour+':'+min+':'+seconds;
 }
@@ -148,4 +91,154 @@ function getDateString( str )
 
     let dateString = month[ d.getMonth() ]+' '+d.getDate()+' '+d.getFullYear()+' '+(hour < 10 ? '0'+hour: hour)+':'+(min<10?'0'+min:min)+' '+m;
     return dateString;
+}
+
+function getCall( call_info )
+{
+	let call_obj = {
+		id				: call_info['qa-call-id']
+		,ticket_id		: call_info['qa-ticket-id']
+		,date			: getUTCMysqlTimestamp( call_info['qa-datetime'] )
+		,local_time		: getMysqlTimestamp( call_info['qa-datetime'] )
+		,to				: call_info['qa-to']
+		,from			: call_info['qa-from']
+		,agent			: call_info['qa-agent']
+		,call_status	: call_info['qa-status']
+		,wait_time		: call_info['qa-wait-time']
+		,hold_time		: call_info['qa-hold-time']
+		,wrap_up_time	: call_info['qa-wrap-up-time']
+		,minutes		: call_info['qa-minutes']
+		,charge			: call_info['qa-charge']
+	};
+
+	return db.get('calls', call_obj.id  ).then(( obj )=>
+	{
+		if( obj !== undefined )
+			return Promise.resolve( obj );
+
+		return Promise.resolve( call_obj );
+	});
+}
+
+function createTicket(call)
+{
+	console.log('Making a http call for create a ticket',call);
+	return Promse.resolve( call );
+	//var promise = axhrw
+	//({
+
+	//	method				: 'POST'			//,'POST',...
+	//	,url				: config.create_ticket_url		//Required
+	//	//,headers			: { "connection"	: config.http_connection }
+	//	,data				:
+	//	{
+	//		title			: 'Missed Call From '+i['qa-from']+' '+date
+	//		,message			: 'Missed Call From '+i['qa-from']+' '+date
+	//					+'\nCall Status: '+i['qa-status']
+	//					+'\nCall To: '+i['qa-to']
+    //                    +'\nWait Time: '+i['qa-wait-time']
+    //                    +'\nHold Time: '+i['qa-hold-time']
+	//	}
+	//})
+
+}
+
+function processCallsFound( requests )
+{
+	if( Array.isArray( request ) )
+	{
+		let calls		= [];
+		let generator	= (call_info)=> getCall(call_info).then((call)=>
+		{
+			if( !call.saved )
+			{
+				call.saved = true;
+				return db.put('calls', call ).then(()=>
+				{
+					return call.call_status === 'Abandoned In Queue'
+						? createTicket( call )
+						: Promise.resolve( call );
+				});
+			}
+			return Promise.resolve( null );
+		});
+
+		PromiseUtil.runSequential( requests, generator ).then((not_saved_calls)=>
+		{
+			let calls_to_save = not_saved_calls.filter( a => a !== null );
+
+			if( calls_to_save.length > 0 )
+			{
+				//TODO
+				console.log('Sending calls to be saved',calls_to_save);
+			}
+		})
+		.catch((e)=>
+		{
+			console.log('Error on proccessing calls', e );
+		});
+	}
+}
+
+function call_was_solved( call )
+{
+	if( 'is_solved' in call && call.is_solved )
+		return Promise.resolve( true );
+
+	return db.getAll('calls',{ index: 'date', '>=': call.from } ).then((calls)=>
+	{
+		let call_resolved =  calls.some( other =>{
+			if( other.id === call.id )
+				return false;
+
+			if( other.to === call.from || other.from === call.from)
+			{
+				if( other.date > call.date && other.call_status == 'Completed' )
+					return true;
+			}
+			return false;
+		});
+
+		return Promise.resolve( call_resolved.length > 0 );
+	});
+}
+
+function getLostCalls()
+{
+	let zero_n		= n => n>9 ? n : '0'+n;
+	let date 		= new Date();
+	date.setHours( date.getHours() -3 );
+
+	let dateString	= getUTCMysqlTimestamp( date.toString() );
+	return db.getAll('calls',{index:'date', '=>': dateString })
+	.then((calls)=>
+	{
+		return Promise.resolve(calls.filter( call => call.call_status === 'Abandoned In Queue' ));
+	});
+}
+
+function makeCallBacks()
+{
+	return getLostCalls().then((lost_calls)=>
+	{
+		let generator = (call)=>
+		{
+			return call_was_solved( call ).then((is_solved)=>
+			{
+				if( !is_solved )
+				{
+					//Send Callback
+					call.is_solved = true;
+					return db.put('calls',call ).then(()=>
+					{
+						console.log('Call Found', call );
+						return Promise.reject('Call Found');
+					});
+				}
+				return Promise.resolve( true );
+			});
+		};
+
+		return PromiseUtil.runSequential( lost_calls, generator );
+	});
 }
